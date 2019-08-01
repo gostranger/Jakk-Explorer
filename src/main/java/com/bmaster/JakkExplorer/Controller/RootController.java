95f9f684328ce2b720bcc7ad6684b4056928d6fa
ghostranger <kiranr0019@gmail.com>
2019-08-02 00:21:41 +0530

    initial commit

---
+++ src/main/java/com/bmaster/JakkExplorer/Controller/RootController.java
@@ -0,0 +1,16 @@
+package com.bmaster.JakkExplorer.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class RootController {
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("UserId", "Ghostranger");
+		return "index.html";
+	}
+}
